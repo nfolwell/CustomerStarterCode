92b0c27726d863bc08f05854482d93ce526333be
Nathan Folwell <nfolwell@schools.nyc.gov>
2020-01-30 07:27:58 -0500

    Starter Code for Customer

---
+++ Customer.java
@@ -0,0 +1,35 @@
+import java.text.DecimalFormat;
+
+public class Customer
+{
+  private static int nextAccNum = 1;
+  private String name;
+  private int currAccNum; 
+  
+  public Customer(String n)
+  {
+    name = n;
+    currAccNum = nextAccNum;
+    nextAccNum++;
+  }
+  
+  public void billNotice(double bill)
+  {
+      
+  }
+  
+  //Write the method to get the next account number
+  //that will be assigned
+  
+  
+  //Write a correct implementation of the updateName method that avoids the error
+  public void updateName(String name)
+  {
+    name = name;
+  }
+  
+  //Write a method to get the customer name
+  
+
+  
+}
---
+++ CustomerRunner.java
@@ -0,0 +1,10 @@
+public class CustomerRunner
+{
+  public static void main(String[] args)
+  {
+     Customer c1 = new Customer("Mr F.");
+     c1.billNotice(2.00);
+     
+      //Add code to test all the methods in Customer
+  }
+}
